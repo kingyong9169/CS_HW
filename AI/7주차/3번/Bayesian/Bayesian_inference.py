8964ee387d72546d497770aac9c84ebf5b6ac063
동동이 <drdragon777@naver.com>
2021-10-23 02:22:46 +0900

    [CHO] - 7주차 3번 bayesian 주석

--- AI/7주차/3번/Bayesian/Bayesian_inference.py
+++ AI/7주차/3번/Bayesian/Bayesian_inference.py
@@ -2,15 +2,17 @@ from pgmpy.inference import VariableElimination
 from Bayesian_register import model
 
 # 베이지안 망의 추론
-infer = VariableElimination(model)
-A_dist = infer.query(['A'])
+infer = VariableElimination(model)  # 모델을 추론한다.
+A_dist = infer.query(['A'])  # A에 대한 추론을 A_dist에 넣는다.
 print('P(A)')
-print(A_dist)
+print(A_dist)  # A_dist 출력
 
-N_I_EF_BT = infer.query(['N'], evidence = {'E': 'F', 'B': 'T'})
+# E=F, B=T에 대하여 N인 경우를 추론한다.
+N_I_EF_BT = infer.query(['N'], evidence={'E': 'F', 'B': 'T'})
 print('P(N | E=F, B=T)')
-print(N_I_EF_BT)
+print(N_I_EF_BT)  # P(N | E=F, B=T)를 출력한다.
 
-N_I_AF_BT = infer.query(['N'], evidence = {'A': 'F', 'B': 'T'})
+# A=F, B=T에 대하여 N인 경우를 추론한다.
+N_I_AF_BT = infer.query(['N'], evidence={'A': 'F', 'B': 'T'})
 print('P(N | A=F, B=T)')
-print(N_I_AF_BT)
+print(N_I_AF_BT)  # P(N | A=F, B=T)를 출력한다.
--- AI/7주차/3번/Bayesian/Bayesian_prob_distribute.py
+++ AI/7주차/3번/Bayesian/Bayesian_prob_distribute.py
@@ -2,27 +2,29 @@ from pgmpy.factors.discrete import TabularCPD
 import numpy as np
 
 # 지진(Earthquake) 발생 확률 분포
-P_E = TabularCPD('E', 2, [[0.9], [0.1]], state_names={'E': ['F', 'T']})
+P_E = TabularCPD('E', 2, [[0.9], [0.1]], state_names={
+                 'E': ['F', 'T']})  # P(E)에서 E가 F, T일 때 확률
 print('P(E)')
-print(P_E)
+print(P_E)  # P(E) 확률 베이지안 출력
 
 # 절도(Burglary) 발생 확률분포
-P_B = TabularCPD('B', 2, [[0.7], [0.3]], state_names={'B': ['F', 'T']})
+P_B = TabularCPD('B', 2, [[0.7], [0.3]], state_names={
+                 'B': ['F', 'T']})  # P(B)에서 B가 F, T일 때 확률
 print('P(B)')
-print(P_B)
+print(P_B)  # P(B) 확률 베이지안 출력
 
 # 경보(Alarm) 발생 확률 분포
 P_A_I_EB = TabularCPD('A', 2, [[0.99, 0.1, 0.3, 0.01], [0.01, 0.9, 0.7, 0.99]],
-            evidence = ['E', 'B'], evidence_card = [2, 2],
-            state_names = {'A': ['F', 'T'], 'E': ['F', 'T'], 'B': ['F', 'T']})
+                      evidence=['E', 'B'], evidence_card=[2, 2],
+                      state_names={'A': ['F', 'T'], 'E': ['F', 'T'], 'B': ['F', 'T']})  # P(A|EB)에서 E가 F, T B가 F, T일 때 확률
 
 print('P(A|EB)')
-print(P_A_I_EB)
+print(P_A_I_EB)  # P(A|EB) 확률 베이지안 출력
 
 # 이웃(Neighbor) 전화 확률 분포
 P_N_I_A = TabularCPD('N', 2,
-            np.array([[0.9, 0.2], [0.1, 0.8]]),
-            evidence = ['A'], evidence_card = [2], 
-            state_names = {'N': ['F', 'T'], 'A': ['F', 'T']})
+                     np.array([[0.9, 0.2], [0.1, 0.8]]),
+                     evidence=['A'], evidence_card=[2],
+                     state_names={'N': ['F', 'T'], 'A': ['F', 'T']})  # P(N|A)에서 A가 F, T일 때 확률
 print('P(N|A)')
-print(P_N_I_A)
+print(P_N_I_A)  # P(N|A) 확률 베이지안 출력
