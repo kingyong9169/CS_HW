838b4773dbb18d2c7316e43e896b3920830e0fdc
동동이 <drdragon777@naver.com>
2021-10-23 02:22:56 +0900

    [CHO] - 7주차 3번 Markov 주석

--- AI/7주차/3번/Markov/Markov_model.py
+++ AI/7주차/3번/Markov/Markov_model.py
@@ -2,10 +2,15 @@ from pgmpy.models import MarkovNetwork
 from pgmpy.factors.discrete import DiscreteFactor
 
 # 마르코프 랜덤 필드(마르코프 모델) 모델 정의
+# 모델의 vertex는 A, B, C, D이다.
 model = MarkovNetwork([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'A')])
-factor1 = DiscreteFactor(['A', 'B'], [2, 2], [30, 5, 1, 10], state_names = {'A': [0,1], 'B': [0,1]})
-factor2 = DiscreteFactor(['B', 'C'], [2, 2], [100, 1, 1, 100], state_names = {'B': [0,1], 'C': [0,1]})
-factor3 = DiscreteFactor(['C', 'D'], [2, 2], [1, 100, 100, 1], state_names = {'C': [0,1], 'D': [0,1]})
-factor4 = DiscreteFactor(['D', 'A'], [2, 2], [100, 1, 1, 100], state_names = {'D': [0,1], 'A': [0,1]})
-model.add_factors(factor1, factor2, factor3, factor4)
-print('모델의 타당성: ', model.check_model())
+factor1 = DiscreteFactor(['A', 'B'], [2, 2], [30, 5, 1, 10], state_names={
+                         'A': [0, 1], 'B': [0, 1]}) # A, B를 연결한 것을 factor1으로 정의
+factor2 = DiscreteFactor(['B', 'C'], [2, 2], [100, 1, 1, 100], state_names={
+                         'B': [0, 1], 'C': [0, 1]}) # B, C를 연결한 것을 factor2으로 정의
+factor3 = DiscreteFactor(['C', 'D'], [2, 2], [1, 100, 100, 1], state_names={
+                         'C': [0, 1], 'D': [0, 1]}) # C, D를 연결한 것을 factor2으로 정의
+factor4 = DiscreteFactor(['D', 'A'], [2, 2], [100, 1, 1, 100], state_names={
+                         'D': [0, 1], 'A': [0, 1]}) # D, A를 연결한 것을 factor2으로 정의
+model.add_factors(factor1, factor2, factor3, factor4) # 모델에 요소 추가
+print('모델의 타당성: ', model.check_model()) # 모델이 타당한지 체크
--- AI/7주차/3번/Markov/Markov_random_field.py
+++ AI/7주차/3번/Markov/Markov_random_field.py
@@ -2,29 +2,29 @@ import numpy as np
 from Markov_model import model
 from pgmpy.inference import VariableElimination
 
-pf_value = model.get_partition_function()
-print('\n분할 함수의 값: ', pf_value)
+pf_value = model.get_partition_function()  # 모델의 분할 함수를 pf_value에 넣는다.
+print('\n분할 함수의 값: ', pf_value)  # 분할 함수 값 출력
 
-infer = VariableElimination(model) # 추론 객체 생성
+infer = VariableElimination(model)  # 추론 객체 생성
 
-phi_ABCD = infer.query(['A', 'B', 'C', 'D']) # 전체 분포 phi(A, B, C, D)
-print('phi(A, B, C, D')
+phi_ABCD = infer.query(['A', 'B', 'C', 'D'])  # 전체 분포 phi(A, B, C, D)
+print('phi(A, B, C, D')  # phi(A, B, C, D) 출력
 print(phi_ABCD)
-P_ABCD = phi_ABCD.values/pf_value # 확률 = (팩터의 곱) / (분할 함수의 값)
+P_ABCD = phi_ABCD.values/pf_value  # 확률 = (팩터의 곱) / (분할 함수의 값)
 PABCD = np.reshape(P_ABCD, -1)
-for val in PABCD: # 확률의 출력
+for val in PABCD:  # 확률의 출력
     print(val, '\n')
 
-A_dist = infer.query(['A']) # A의 분포 phi(A)
-print('phi(A)')
+A_dist = infer.query(['A'])  # A의 분포 phi(A)
+print('phi(A)') # phi(A) 출력
 print(A_dist)
-P_A = A_dist.values/np.sum(A_dist.values)
-for val in P_A:
+P_A = A_dist.values/np.sum(A_dist.values) # 확률 = (팩터의 곱) / (분할 함수의 값)
+for val in P_A: # 확률 출력
     print(val, '\n')
 
-AIBOC1_dist = infer.query(['A'], evidence = {'B':0, 'C':1}) # phi(A|B=0, C=1)
-print('phi(A|B=0, C=1)')
+AIBOC1_dist = infer.query(['A'], evidence={'B': 0, 'C': 1})  # phi(A|B=0, C=1)
+print('phi(A|B=0, C=1)') # phi(A|B=0, C=1) 출력
 print(AIBOC1_dist)
-P_AIBOC1 = AIBOC1_dist.values/np.sum(AIBOC1_dist.values)
-for val in P_AIBOC1:
+P_AIBOC1 = AIBOC1_dist.values/np.sum(AIBOC1_dist.values) # 확률 = (팩터의 곱) / (분할 함수의 값)
+for val in P_AIBOC1: # 확률 출력
     print(val, '\n')
