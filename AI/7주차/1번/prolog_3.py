3e3522631c165a839f26555185b7e0b9c3eca19d
동동이 <drdragon777@naver.com>
2021-10-23 02:21:55 +0900

    [CHO] - 7주차 1번 Prolog3 주석

--- AI/7주차/1번/prolog_3.py
+++ AI/7주차/1번/prolog_3.py
@@ -1,13 +1,15 @@
 from pyswip import *
 
-N = 3
+N = 3  # 원반 개수
 
-def notify(t):
-    print("%s --> %s" % tuple(t))
+
+def notify(t):  # notify 함수
+    print("%s --> %s" % tuple(t))  # tuple(t)의 첫 번째, 두 번째 값을 출력한다.
+
+
 notify.arity = 1
 
-prolog = Prolog()
-registerForeign(notify)
-prolog.consult("hanoi.pl")
-list(prolog.query("hanoi(%d)" % N))
-
+prolog = Prolog()  # 프롤로그 생성
+registerForeign(notify)  # 외부 함수 prolog에 등록
+prolog.consult("AI/7주차/1번/hanoi.pl")  # prolog 프로그램 파일 읽어들이기
+list(prolog.query("hanoi(%d)" % N))  # hanoi(3)을 질의하여 출력한다.
